bd1f76ea8fb6b65a469228bb25a5715f41198994
Kanishk Singh Maurya <95866249+kanishk466@users.noreply.github.com>
2024-12-29 11:53:04 +0530

    Update server.js add cors

--- server.js
+++ server.js
@@ -4,12 +4,14 @@ import { configDotenv } from "dotenv";
 import ConnectDB  from "./src/config/db.js"
 import authRoutes from "./src/routes/auth.route.js";
 import postsRoutes from "./src/routes/post.route.js"
+import cors from "cors";
 
 configDotenv();
 const app = express();
 app.use(bodyParser.json())
 
 app.use(express.json());
+app.use(cors());
 
 ConnectDB();
 
